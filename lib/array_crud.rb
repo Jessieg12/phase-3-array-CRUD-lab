11a4311c5a3f06aaba016965662fbe8ec290c017
Jessie Guzman <jessieguzman777@gmail.com>
2023-11-01 09:57:34 -0500

    commit tree f69f159842dfaef81be1bf3b5167a90341a73f78

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,35 +1,44 @@
+require "pry"
+
 def create_an_empty_array
-  
+  empty_array = []
+  return empty_array
 end
 
 def create_an_array
-  
+  array = [1, 2, 3, 4]
+  return array
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  return array << element
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  return array.unshift(element)
 end
 
 def remove_element_from_end_of_array(array)
-  
+  return array.pop
 end
 
 def remove_element_from_start_of_array(array)
-  
+  return array.shift 
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+  return array.delete_at(index_number)
 end
 
 def retrieve_first_element_from_array(array)
-  
+  first = array.find do |element|
+    element[0]
+  end
+  return first
 end
 
 def retrieve_last_element_from_array(array)
-  
+  return array.last
+  binding.pry
 end
+binding.pry
